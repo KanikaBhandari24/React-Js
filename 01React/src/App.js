7ff02a6c574a621dbab0eda7fafc1dcab67dc280
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-03-02 08:12:12 +0530

    Add files via upload

---
+++ 01React/src/App.js
@@ -0,0 +1,12 @@
+import code from './code'
+function App() {
+  return (
+    <>
+    <code/>
+    <h1>Hello programmers</h1>
+    </>
+  );
+}
+
+export default App;
+ 
---
+++ 01React/src/code.js
@@ -0,0 +1,9 @@
+//self created file
+
+function code(){
+    return (
+        <h2> Hello programmers</h2>
+    )
+
+}
+export default code 
